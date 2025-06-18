92f7a3dbe8d1a27279d54aa79a3b0106cbd5cfce
Jose L Nogueira <joseluisnogueirajr@gmail.com>
2025-06-18 14:01:42 -0300

    apaguei coisa

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("This is a simple C# program.");
-Console.WriteLine("It prints messages to the console.");   
-
 Console.WriteLine("Press any key to exit...");
