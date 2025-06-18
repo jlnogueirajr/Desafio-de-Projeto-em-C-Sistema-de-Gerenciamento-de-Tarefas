5f3011b60c12808451c65d2c44774639c7a7a6d6
Jose L Nogueira <joseluisnogueirajr@gmail.com>
2025-06-18 14:05:09 -0300

    coloquei uma chafe key

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("This is a simple C# program.");
 Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+// This program prints a greeting message to the console and waits for user input before exiting.
