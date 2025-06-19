77510c4a835147c4f411193bb9a3f871f75ee625
Wesley <wesleycleiton2502@hotmail.com>
2025-06-19 08:08:20 -0300

    Adicionando duas classes uma Tarefas e um Gerenciador

---
+++ Modulos/Gerenciador.cs
@@ -0,0 +1,25 @@
+﻿namespace Projeto.Modulos
+{
+    public class Gerenciador
+    {
+        List<Tarefa> tarefas = new List<Tarefa>();//Lista para aarmazenar as tarefas
+        int Id = 1;
+
+        //Funçao para adicionar uma tarefa, que contem ID, DESCRIÇÂO e STATUS na criação
+        public void AdicionarTarefa(string descricao)
+        {
+            Tarefa adicionarTarefa = new Tarefa(Id, descricao, "Pendente");
+            tarefas.Add(adicionarTarefa);
+            Id++;
+        }
+
+        //função para mostrar as tarefas adiocionadas
+        public void ListarTarefas()
+        {
+            foreach (Tarefa tarefa in tarefas)//laço de repetição onde ao ser chamada mostra a tarefa for ID e Descrição
+            {
+                Console.WriteLine($"{tarefa.Id}. {tarefa.Descricao}");
+            }
+        }
+    }
+}
---
+++ Modulos/Tarefas.cs
@@ -0,0 +1,18 @@
+﻿namespace Projeto.Modulos
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Status{ get; set; }
+        public DateTime DataCriacao { get; set; }
+
+        public Tarefa(int id, string descricao, string status)
+        {
+            Id = id;
+            Descricao = descricao;
+            Status = status;
+            DataCriacao = DateTime.Now;//A data da tarefa é preenchida com a data Atual.
+        }
+    }
+}
